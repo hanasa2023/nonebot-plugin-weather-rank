2b4a0ffd983980ab56ed12fddda2ecd5110b9445
hanasa2023 <hanasakayui2022@gmail.com>
2024-09-14 12:44:52 +0800

    ✅ Add test

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import os
+
+import nonebot
+import pytest
+from nonebot.adapters.onebot.v11 import Adapter
+from nonebug import NONEBOT_INIT_KWARGS
+
+
+def pytest_configure(config: pytest.Config):
+    config.stash[NONEBOT_INIT_KWARGS] = {
+        'driver': '~fastapi',
+        'log_level': 'DEBUG',
+        'host': '127.0.0.1',
+        'port': '9555',
+    }
+    os.environ['PLUGIN_ALCONNA_TESTENV'] = '1'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_bot():
+    # 加载适配器
+    driver = nonebot.get_driver()
+    driver.register_adapter(Adapter)
+
+    # 加载插件
+    nonebot.load_from_toml('pyproject.toml')
---
+++ tests/test_weather_rank.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.mark.asyncio()
+async def test_get_map() -> None:
+    import re
+
+    import httpx
+    from bs4 import BeautifulSoup, Tag
+
+    url = ''
+    async with httpx.AsyncClient() as ctx:
+        res = await ctx.get(
+            'http://www.nmc.cn/publish/observations/hourly-temperature.html'
+        )
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, 'html.parser')
+            img_element: Tag | None = soup.select_one('.imgblock > img')
+            if img_element:
+                img_url: str | list[str] | None = img_element.get('src')
+                if isinstance(img_url, str):
+                    url = img_url
+    url_pattern = re.compile(r'^(https?|ftp)://[^\s/$.?#].[^\s]*$')
+    assert url_pattern.match(url), f"URL '{url}' is not a valid web link"
