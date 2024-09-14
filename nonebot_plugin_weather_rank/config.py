d7c64554bb8802b83f2674141f08ea17993f474f
hanasa2023 <hanasakayui2022@gmail.com>
2024-09-14 10:26:40 +0800

    ✨ Support standard/pay subscribe

--- nonebot_plugin_weather_rank/config.py
+++ nonebot_plugin_weather_rank/config.py
@@ -6,6 +6,7 @@ class Config(BaseModel):
     """Plugin Config Here"""
 
     qweather_api_key: str = ''
+    qweather_rank_mode: int = 0
 
 
 plugin_config: Config = get_plugin_config(Config)
--- nonebot_plugin_weather_rank/utils/constant.py
+++ nonebot_plugin_weather_rank/utils/constant.py
@@ -1,7 +1,25 @@
-NOW_WEATHER_SEARCH_BASE_URL = 'https://devapi.qweather.com/v7/weather/now?'
-DAILY_WEATHER_SEARCH_BASE_URL = 'https://devapi.qweather.com/v7/weather/7d?'
-HOURLY_WEATHER_SEARCH_BASE_URL = 'https://devapi.qweather.com/v7/weather/24h?'
-AIR_QUALITY_BASE_URL = 'https://devapi.qweather.com/v7/air/now?'
+from ..config import plugin_config
+
+NOW_WEATHER_SEARCH_BASE_URL = (
+    'https://devapi.qweather.com/v7/weather/now?'
+    if plugin_config.qweather_rank_mode == 0
+    else 'https://api.qweather.com/v7/weather/now?'
+)
+DAILY_WEATHER_SEARCH_BASE_URL = (
+    'https://devapi.qweather.com/v7/weather/7d?'
+    if plugin_config.qweather_rank_mode == 0
+    else 'https://api.qweather.com/v7/weather/7d?'
+)
+HOURLY_WEATHER_SEARCH_BASE_URL = (
+    'https://devapi.qweather.com/v7/weather/24h?'
+    if plugin_config.qweather_rank_mode == 0
+    else 'https://api.qweather.com/v7/weather/24h?'
+)
+AIR_QUALITY_BASE_URL = (
+    'https://devapi.qweather.com/v7/air/now?'
+    if plugin_config.qweather_rank_mode == 0
+    else 'https://api.qweather.com/v7/air/now?'
+)
 CITY_SEARCH_BASE_URL = 'https://geoapi.qweather.com/v2/city/lookup?'
 TEMPERATURE_MAP_BASE_URL = (
     'http://www.nmc.cn/publish/observations/hourly-temperature.html'
