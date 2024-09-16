08c85f43d71bf3ec078d131e4ae8a62f24f38c65
hanasa2023 <hanasakayui2022@gmail.com>
2024-09-17 02:01:58 +0800

    💄 Update UI by Material Design

--- nonebot_plugin_weather_rank/__init__.py
+++ nonebot_plugin_weather_rank/__init__.py
@@ -4,11 +4,11 @@ from .config import Config
 from .utils.schema import Weather  # noqa: F401
 from .weather_rank import weather_rank, weather_rank_helper  # noqa: F401
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __plugin_meta__ = PluginMetadata(
     name='weather-rank',
-    description='订阅地区的天气排行榜',
-    usage='获取气温/昼夜温差排行榜、获取气温图、当地天气',
+    description='显示已订阅地区的天气排行榜',
+    usage='获取气温/昼夜温差排行榜、获取气温图、当地天气、天气帮助',
     type='application',
     homepage='https://github.com/hanasa2023/nonebot-plugin-weather-rank#readme',
     config=Config,
